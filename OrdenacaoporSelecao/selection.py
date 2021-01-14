1f6602fa64a9145c2012cd2734c6684c09ba8474
Alan Oliveira <alancjobm@gmail.com>
2021-01-13 23:27:11 -0300

    my sort

--- OrdenacaoporSelecao/selection.py
+++ OrdenacaoporSelecao/selection.py
@@ -5,6 +5,12 @@ def minorSearch(arr):
         if arr[i] < smaller:
             smaller = arr[i]
             lowest_index = i 
+    return lowest_index
 
 def ordination(arr):
-    
+    array = arr.copy()
+    newArray = list()
+    for _ in range(len(array)):
+        index = minorSearch(array)
+        newArray.append(array.pop(index))
+    return newArray
