bfab247359df1217bea844eccc9098b53b537716
Alan Oliveira <alancjobm@gmail.com>
2021-02-08 21:29:49 -0300

    retomando

---
+++ ordernacao.py
@@ -0,0 +1,26 @@
+
+
+class OrdenacaoSelecao:
+
+    def __buscaMenor(self, arr):
+        menor = arr[0]
+        menor_indice = 0
+
+        for i in range(1, len(arr)):
+            if arr[i] < menor:
+                menor = arr[i]
+                menor_indice = i 
+        return menor_indice 
+
+    def selecao(self, arr):
+        novoArr = []
+        for i in range(len(arr)):
+            menor = self._OrdenacaoSelecao__buscaMenor(arr)
+            novoArr.append(arr.pop(menor))
+        return novoArr
+    
+
+lista = [2, 10, 1, 34]
+
+order = OrdenacaoSelecao()
+print(order.selecao(lista))
